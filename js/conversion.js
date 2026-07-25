6413e05aa2ce0a5088a19fa80d0f69fd758ab5b5
vikas <creative.boy.vk@gmail.com>
2026-07-25 11:25:59 +0530

    Implement back navigation on back-title click
    Add click event listener to back-title elements for navigation.
    
    Signed-off-by: vikas <creative.boy.vk@gmail.com>

--- js/conversion.js
+++ js/conversion.js
@@ -193,7 +193,15 @@ function tan(deg) {
 function sqrt(num) { 
     return Math.sqrt(num); 
 }
-
+document.querySelectorAll(".back-title").forEach(el => {
+        el.addEventListener("click", () => {
+            if (history.length > 1) {
+                history.back();
+            } else {
+                window.location.href = "landing.html"; // fallback page
+            }
+        });
+    });
 function calculateResult() {
     try {
         let expression = calcDisplay.value;
