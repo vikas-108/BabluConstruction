b858e32c7c470150bab5b15435cba26897a7f0ae
vikas <creative.boy.vk@gmail.com>
2026-08-09 23:17:43 +0530

    Update API_BASE_URL for production environment
    Signed-off-by: vikas <creative.boy.vk@gmail.com>

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // ---- Edit these two to match your setup ----
 
 // Base URL of your Express server's API
-const API_BASE_URL = "http://localhost:5000/api/client/create";
-//const API_BASE_URL = "https://api.buildskil.com/api/client/create";
+//const API_BASE_URL = "http://localhost:5000/api/client/create";
+const API_BASE_URL = "https://api.buildskil.com/api/client/create";
 
 // The localStorage key your login flow already saves the JWT under.
 // e.g. after login you probably do: localStorage.setItem("token", data.token)
