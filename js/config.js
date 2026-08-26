f225f962cda4445f3cfa80d3dc114dd86a4fea4c
vikas <creative.boy.vk@gmail.com>
2026-08-26 20:17:33 +0530

    Update config.js
    Signed-off-by: vikas <creative.boy.vk@gmail.com>

--- js/config.js
+++ js/config.js
@@ -3,7 +3,8 @@
 // Base URL of your Express server's API
 //window.API_BASE_URL = "http://localhost:5000/api/client/create";
 window.API_BASE_URL = "https://api.buildskil.com/api/client/create";
-
+//window.PROFILE_API_BASE_URL = "http://localhost:5000/api/profiles/public";
+window.PROFILE_API_BASE_URL = "https://api.buildskil.com/api/profiles/public";
 // The localStorage key your login flow already saves the JWT under.
 // e.g. after login you probably do: localStorage.setItem("token", data.token)
 window.AUTH_TOKEN_KEY = "cb_token";
